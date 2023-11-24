d3ccc5b6d6e520c2d283613fcef295e70a5b6f18
Shawky Ebrahim Ahmed <101745968+shawkyebrahim2514@users.noreply.github.com>
2023-11-24 19:36:46 +0200

    Upload the main structure of the library class

--- librarySystem.cpp
+++ librarySystem.cpp
@@ -2,29 +2,52 @@
 using namespace std;
 
 // Library class to manage all methods
-class Library {
+class Library
+{
 private:
     // Save author information
-    struct Author {
+    struct Author
+    {
         char authorID[15];
         char authorName[30];
         char address[30];
     };
     // Save book information
-    struct Book {
+    struct Book
+    {
         char ISBN[15];
         char bookTitle[30];
         char authorID[15];
     };
 
+    const string authorsFile = "authors.txt";                                                 // Authors data file
+    const string authorsPrimaryIndexFile = "authorsPrimaryIndex.txt";                         // Authors primary index file
+    const string authorsSecondaryIndexFile = "authorsSecondaryIndex.txt";                     // Authors secondary index file
+    const string authorsSecondaryIndexLinkedListFile = "authorsSecondaryIndexLinkedList.txt"; // Authors secondary index linked list file
+
+    const string booksFile = "books.txt";                                                 // Books data file
+    const string booksPrimaryIndexFile = "booksPrimaryIndex.txt";                         // Books primary index file
+    const string booksSecondaryIndexFile = "booksSecondaryIndex.txt";                     // Books secondary index file
+    const string booksSecondaryIndexLinkedListFile = "booksSecondaryIndexLinkedList.txt"; // Books secondary index linked list file
+
+    const char lengthDelimiter = '|'; // Delimiter to separate length indicator from record data
+
+    map<char[15], int> authorsPrimaryIndex;   // (Author ID, byte offset) Authors primary index
+    map<char[30], int> authorsSecondaryIndex; // (Author Name, First record in linked list) Authors secondary index
+
+    map<char[15], int> booksPrimaryIndex;   // (ISBN, byte offset) Books primary index
+    map<char[15], int> booksSecondaryIndex; // (Author ID, First record in linked list) Books secondary index
+
 public:
     // Add a new author
-    void addAuthor() {
+    void addAuthor()
+    {
         Author newAuthor;
         // Open the authors data file to add a new author
-        fstream authors("authors.txt", ios::app | ios::binary);
+        fstream authors(authorsFile, ios::app | ios::binary);
         // Check if the file has successfully opened without errors
-        if(authors.fail()) {
+        if (authors.fail())
+        {
             cerr << "\tError opening the file!\n";
             return;
         }
@@ -51,13 +74,23 @@ public:
         authors.close();
     }
 
+    void addAuthorToDataFile(Author author) {}
+
+    void addAuthorToPrimaryIndexFile(Author author, int byteOffest) {}
+
+    void addAuthorToSecondaryIndexFile(Author author) {}
+
+    void addAuthorToSecondaryIndexLinkedListFile(Author author) {}
+
     // Add a new book
-    void addBook() {
+    void addBook()
+    {
         Book newBook;
         // Open the books data file to add a new book
-        fstream books("books.txt", ios::app | ios::binary);
+        fstream books(booksFile, ios::app | ios::binary);
         // Check if the file has successfully opened without errors
-        if(books.fail()) {
+        if (books.fail())
+        {
             cerr << "\tError opening the file!\n";
             return;
         }
@@ -84,11 +117,64 @@ public:
         books.close();
     }
 
+    void addBookToDataFile(Book book) {}
+
+    void addBookToPrimaryIndexFile(Book book, int byteOffest) {}
+
+    void addBookToSecondaryIndexFile(Book book) {}
+
+    void addBookToSecondaryIndexLinkedListFile(Book book) {}
+
+    // Update author name using author ID
+    void updateAuthorName() {}
+
+    void updateAuthorNameInDataFile(char newAuthorName[3], int byteOffset) {}
+
+    void updateAuthorNameInSecondayIndex(char oldAuthorName[30], char newAuthorName[30]) {}
+
+    // Update book title using ISBN
+    void updateBookTitle() {}
+
+    void updateBookTitleInDataFile(char newBookTitle[30], int byteOffset) {}
+
+    // Delete an author using author ID
+    void deleteAuthor() {}
+
+    void deleteFromAuthorsDataFile(int byteOffset) {}
+
+    void deleteFromAuthorsPrimaryIndexFile(char authorId[30]) {}
+
+    void deleteFromAuthorsSecondaryIndexFile(char authorName[30]) {}
+
+    void deleteFromAuthorsSecondaryIndexLinkedListFile(int firstPosition, char authorId[30]) {}
+
+    // Delete a book using ISBN
+    void deleteBook() {}
+
+    void deleteFromBooksDataFile(int byteOffset) {}
+
+    void deleteFromBooksPrimaryIndexFile(char ISBN[15]) {}
+
+    void deleteFromBooksSecondaryIndexFile(char authorId[30]) {}
+
+    void deleteFromBooksSecondaryIndexLinkedListFile(int firstPosition, char ISBN[15]) {}
+
+    // Print author using author ID
+    void printAuthor() {}
+
+    // Print book using ISBN
+    void printBook() {}
+
+    // Handle the select query
+    void writeQuery() {}
+
     // Main method to start the program
-    void start() {
+    void start()
+    {
         cout << "\tWelcome to the Library Catalog System!\n";
         // Loop until user exits
-        while (true) {
+        while (true)
+        {
             cout << "\n1. Add New Author\n"
                     "2. Add New Book\n"
                     "3. Update Author Name (Author ID)\n"
@@ -102,16 +188,23 @@ public:
             cout << "Please Enter Choice (1-10) ==> ";
             string choice;
             getline(cin, choice);
-            if (choice == "1") {
+            if (choice == "1")
+            {
                 cout << "\tAdding New Author...\n";
                 addAuthor();
-            } else if (choice == "2") {
+            }
+            else if (choice == "2")
+            {
                 cout << "\tAdding New Book...\n";
                 addBook();
-            } else if (choice == "10") {
+            }
+            else if (choice == "10")
+            {
                 cout << "\tThank you for using our Library System!\n";
                 break;
-            } else {
+            }
+            else
+            {
                 cerr << "\n\tINVALID CHOICE!!\n";
             }
         }
@@ -119,7 +212,8 @@ public:
 };
 
 // Start the program
-int main() {
+int main()
+{
     Library lib;
     lib.start();
     return 0;
