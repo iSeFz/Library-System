a3d6e1682fdb33bdf6e8d229953a1abd2edc69a1
iSeFz <seifyahia@gmail.com>
2023-11-27 16:33:24 +0200

    Added methods to create, load & save authors primary index file In order to insure the consistency of data throughout the program

--- librarySystem.cpp
+++ librarySystem.cpp
@@ -41,6 +41,121 @@ private:
     map<char[15], int> booksSecondaryIndex; // (Author ID, First record in linked list) Books secondary index
 
 public:
+    // Create the author primary index file
+    void createAuthorPrimaryIndex()
+    {
+        // Open the data file in input mode
+        fstream authors(authorsFile, ios::in | ios::binary);
+
+        // Skip the header value
+        authors.seekg(2, ios::beg);
+
+        // Create the index file
+        while (authors)
+        { // Read the record size to be able to jump to the end of the record
+            short recordSize;
+            authors.read((char *)&recordSize, sizeof(short));
+            // Store the byte offset of the current record
+            short tempOffset = authors.tellg();
+            if (tempOffset == -1) // If the file has no records, just the header
+                break;
+            // Read the value of author id
+            int tempID = 0;
+            char authorID[15];
+            authors.read((char *)&authorID, sizeof(authorID));
+            // Convert the authorID from character array into integer
+            for (int i = 0; authorID[i] != '\0' and authorID[i] != lengthDelimiter; i++)
+            {
+                tempID *= 10;
+                tempID += (authorID[i] - '0');
+            }
+            // Jump to the next record, minus 17 to compensate the read of the record size itself (2 bytes)
+            // And the rest (15 bytes) to compensate the dummy read of authorID character array
+            authors.seekg(recordSize - 17, ios::cur);
+            // Insert the record into the map to be sorted in memory by the author id
+            authorsPrimaryIndex.insert({tempID, tempOffset});
+        }
+        authors.close();
+
+        // Open the file in output mode
+        fstream authorPrimIdx(authorsPrimaryIndexFile, ios::out | ios::binary);
+
+        // Write the status flag at the beginning of the file
+        // Before appending any records in it
+        isAuthorPrimIdxUpToDate = 0;
+        authorPrimIdx.seekp(0, ios::beg);
+        authorPrimIdx.write((char *)&isAuthorPrimIdxUpToDate, sizeof(short));
+        authorPrimIdx.close();
+
+        // Save the index file to disk
+        saveAuthorPrimaryIndex();
+    }
+
+    // Retrieve data from the map & write it back to the physical file on disk
+    void saveAuthorPrimaryIndex()
+    {
+        // Open the file in multiple modes
+        fstream authorPrimIdx(authorsPrimaryIndexFile, ios::in | ios::out | ios::binary);
+
+        // Read the status flag
+        authorPrimIdx.seekg(0, ios::beg);
+        authorPrimIdx.read((char *)&isAuthorPrimIdxUpToDate, sizeof(short));
+
+        // If the file is already up to date, do not write & exit
+        if (isAuthorPrimIdxUpToDate == 1)
+            return;
+
+        // Otherwise if the file is not up to date OR it is the first time to save it, write it to disk
+        // Update the index file by rewriting it back to disk after inserting into the map
+        for (auto record : authorsPrimaryIndex)
+        {
+            authorPrimIdx.write((char *)&record.first, sizeof(int));    // Write the authorID
+            authorPrimIdx.write((char *)&record.second, sizeof(short)); // Write the byte offset
+        }
+
+        // Update the file status to be up to date, by setting isAuthorPrimIdxUpToDate to 1
+        isAuthorPrimIdxUpToDate = 1;
+        authorPrimIdx.seekp(0, ios::beg);
+        authorPrimIdx.write((char *)&isAuthorPrimIdxUpToDate, sizeof(short));
+        authorPrimIdx.close();
+    }
+
+    // Load authors primary index file into memory
+    void loadAuthorPrimaryIndex()
+    {
+        // Open the index file in input mode
+        fstream authorPrimIdx(authorsPrimaryIndexFile, ios::in | ios::binary);
+
+        // Get the file size, store its offset in endOffset
+        authorPrimIdx.seekg(0, ios::end);
+        short endOffset = authorPrimIdx.tellg();
+
+        // Check the status field
+        authorPrimIdx.seekg(0, ios::beg);
+        authorPrimIdx.read((char *)&isAuthorPrimIdxUpToDate, sizeof(short));
+
+        // If the file is outdated, recreate it
+        if (isAuthorPrimIdxUpToDate != 1)
+        {
+            createAuthorPrimaryIndex();
+            return;
+        }
+
+        // Insert all records into the authors primary index map
+        while (authorPrimIdx)
+        { // If reached the end of file, exit
+            if (authorPrimIdx.tellg() == endOffset)
+                break;
+            int tempID;
+            short tempOffset;
+            authorPrimIdx.read((char *)&tempID, sizeof(int));
+            authorPrimIdx.read((char *)&tempOffset, sizeof(short));
+            // Insert the record into the map to be sorted in memory by the author id
+            authorsPrimaryIndex.insert({tempID, tempOffset});
+        }
+        authorPrimIdx.close();
+    }
+
     // Add a new author helper function
     void addAuthor()
     {
@@ -254,6 +369,8 @@ public:
     void start()
     {
         cout << "\tWelcome to the Library Catalog System!\n";
+        // Load index files into memory
+        loadAuthorPrimaryIndex();
         // Loop until user exits
         while (true)
         {
@@ -282,7 +399,7 @@ public:
             }
             else if (choice == "10")
             {
-                cout << "\tThank you for using our Library System!\n";
+                terminate();
                 break;
             }
             else
@@ -290,6 +407,14 @@ public:
                 cerr << "\n\tINVALID CHOICE!!\n";
             }
         }
+    }
+
+    // Terminate the program correctly with saved changes
+    void terminate()
+    {
+        // Save the primary index files to the disk with the updated indices
+        saveAuthorPrimaryIndex();
+        cout << "\tThank you for using our Library System!\n";
     }
 };
 
