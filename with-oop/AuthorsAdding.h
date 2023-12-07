5ca45ca558ba71d930c0d8ec052059a034497ed1
iSeFz <seifyahia@gmail.com>
2023-12-07 13:58:03 +0200

    Added checks for Author ID to avoid duplication When adding a new author verify it is unique Also added the avail list addition to authors data file Also added header guides + some comments

--- with-oop/AuthorsAdding.h
+++ with-oop/AuthorsAdding.h
@@ -1,8 +1,6 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <map>
-#include <cstring>
+#ifndef AUTHORS_ADDING_H
+#define AUTHORS_ADDING_H
+
 #include "LibraryUtilities.h"
 #include "Author.h"
 
@@ -14,8 +12,19 @@ public:
     {
         // Get author data from the user & store it in an Author object
         Author newAuthor;
-        cout << "Enter author ID: ";
-        cin.getline(newAuthor.authorID, 15);
+        while (true)
+        { // Check for the author id to verify that it is unique
+            cout << "Enter author ID: ";
+            cin.getline(newAuthor.authorID, 15);
+            long long authID = LibraryUtilities::convertCharArrToLongLong(newAuthor.authorID);
+            // Search for the given author id
+            auto result = authorsPrimaryIndex.lower_bound(authID);
+            // If the id is found, print a warning message indicating that it is a duplicate
+            if (result != authorsPrimaryIndex.end() && result->first == authID)
+                cerr << "\tAn Author with the same ID already exists\n";
+            else // Otherwise, break the loop & continue data entry
+                break;
+        }
         cout << "Enter author Name: ";
         cin.getline(newAuthor.authorName, 30);
         cout << "Enter address: ";
@@ -32,7 +41,7 @@ public:
         fstream authors(LibraryUtilities::authorsFile, ios::in | ios::out | ios::app | ios::binary);
 
         // Read the header of the file
-        authors.seekg(0);
+        authors.seekg(0, ios::beg);
         authors.read((char *)&header, sizeof(header));
 
         // Get the sizes of the id, name & address
@@ -46,8 +55,8 @@ public:
         recordSize = idSize + nameSize + addSize + 5;
 
         // If the avail list is empty, insert at the end of the file
-        // if (header == -1)
-        // {
+        if (header == -1)
+        {
             authors.seekp(0, ios::end); // Seek to the end of file
             authors.write((char *)&recordSize, sizeof(recordSize));
             short offset = authors.tellp(); // Store the byteoffset of the new record
@@ -58,16 +67,122 @@ public:
             authors.write((char *)&LibraryUtilities::lengthDelimiter, 1);
             authors.write(author.address, addSize);
             authors.write((char *)&LibraryUtilities::lengthDelimiter, 1);
-            // Add the new author to the primary index file
+            // Add the new author to index files
             addAuthorToPrimaryIndexFile(author.authorID, offset, authorsPrimaryIndex);
             addAuthorToSecondaryIndexFile(author, authorsSecondaryIndex);
             cout << "\tNew Author Added Successfully!\n";
             authors.close();
-        // }
+        }
+        else
+        { // Otherwise, if the avail list is NOT empty
+            short prevOffset = header, deletedSize, differSize, tempOffset;
+            bool fragmentation = false;
+            char deleteChar;
+
+            // Seek to the offset of the last deleted record
+            authors.seekg(header, ios::beg);
+            // First fit algorithm to find the first large enough record to insert the new record into
+            while (true)
+            {
+                tempOffset = prevOffset; // Store the previous offset before overriding its value
+                // Read the first character of the record to check if it's REALLY a deleted record or not
+                authors.read((char *)&deleteChar, 1);
+                // If it's NOT a deleted record, break the loop
+                if (deleteChar != '*')
+                { // Fake the differSize, in order to force insert the new record at the end of the file
+                    differSize = -1;
+                    header = -1;
+                    // Seek to the beginning of file & update the header to be -1
+                    authors.seekp(0, ios::beg);
+                    authors.write((char *)&header, sizeof(header));
+                    break;
+                }
+                // Otherwise, continue parsing the record
+                authors.ignore(1);                                       // Ignore the delimiter '|'
+                authors.read((char *)&prevOffset, sizeof(prevOffset));   // Read the offset of the previous deleted record
+                authors.ignore(1);                                       // Ignore the delimiter '|'
+                authors.read((char *)&deletedSize, sizeof(deletedSize)); // Read the size of the current deleted record
+
+                // Calculate the difference in size between the deleted record & the new record to insert
+                // In order to decide the exact position to insert the new record
+                differSize = deletedSize - recordSize;
+
+                // If the difference is positive, means the current deleted record is suitable to insert in
+                // As the size of the new record is smaller than the deleted record
+                if (differSize >= 0)
+                { // If this is NOT the first deleted record
+                    if (prevOffset != -1)
+                    {
+                        // Update the header to be the offset of the previous deleted record
+                        authors.seekp(0, ios::beg);
+                        authors.write((char *)&prevOffset, sizeof(prevOffset));
+                    }
+                    break;
+                }
+
+                // If this is the first deleted record & no other records before it satisfy the condition of size
+                // Break the loop to insert at the end of the file and keep the header as it is
+                if (prevOffset == -1)
+                    break;
+
+                // Otherwise, continue the loop
+                authors.seekg(prevOffset, ios::beg);
+            }
+
+            // If the difference is negative, then the new record is bigger than the deleted one
+            if (differSize < 0)
+                // Insert at the end of the file and keep the header value as it is
+                authors.seekp(0, ios::end);
+            else
+            { // Otherwise, the difference is positive, then the new record size is smaller than the deleted one
+                // Seek to the deleted record offset to write the new record
+                authors.seekp(tempOffset, ios::beg);
+                // Add the difference in size to the current record size in order to apply internal fragmentation
+                recordSize += differSize;
+                // Flag to indicate that internal fragmentation is required
+                // Check if the sizes are equal (meaning the differSize is zero), then do NOT apply fragmentation
+                fragmentation = (differSize == 0) ? false : true;
+            }
+
+            // Write the record size of the new record
+            authors.write((char *)&recordSize, sizeof(recordSize));
+            short offset = authors.tellp(); // Store the byteoffset of the new record
+            // Write the rest of record fields separated by delimeters
+            authors.write(author.authorID, idSize);
+            authors.write((char *)&LibraryUtilities::lengthDelimiter, 1);
+            authors.write(author.authorName, nameSize);
+            authors.write((char *)&LibraryUtilities::lengthDelimiter, 1);
+            authors.write(author.address, addSize);
+            authors.write((char *)&LibraryUtilities::lengthDelimiter, 1);
+
+            // Check for internal fragmentation
+            if (fragmentation)
+            { // Apply internal fragmentation and fill the rest of record space with telda '~'
+                short iterator = authors.tellp();
+                while (iterator < (tempOffset + recordSize - 2) && tempOffset != -1)
+                {
+                    authors.write("~", 1);
+                    iterator++;
+                }
+                // Write the last delimiter after the telda to indicate the end of this record
+                authors.write((char *)&LibraryUtilities::lengthDelimiter, 1);
+            }
+
+            // Read the header of the file again to get the latest update
+            authors.seekg(0, ios::beg);
+            authors.read((char *)&header, sizeof(header));
+            // Fix the avail list pointers to avoid corrupted pointers
+            LibraryUtilities::fixAvailList(header, LibraryUtilities::authorsFile);
+            // Add the new author to index files
+            addAuthorToPrimaryIndexFile(author.authorID, offset, authorsPrimaryIndex);
+            addAuthorToSecondaryIndexFile(author, authorsSecondaryIndex);
+            cout << "\tNew Author Added Successfully!\n";
+            authors.close();
+        }
     }
 
     // Add the new author to the primary index file
-    void addAuthorToPrimaryIndexFile(char authorID[], short byteOffset, map<long long, short> &authorsPrimaryIndex)
+    void addAuthorToPrimaryIndexFile(char authorID[], short &byteOffset, map<long long, short> &authorsPrimaryIndex)
     {
         // Convert the authorID from character array into long long
         long long authID = LibraryUtilities::convertCharArrToLongLong(authorID);
@@ -79,25 +194,29 @@ public:
         LibraryUtilities::markAuthorsPrimaryIndexFlag('0');
     }
 
-    void addAuthorToSecondaryIndexFile(Author author, map<string, short> &authorsSecondaryIndex)
+    // Add the new author to the secondary index file
+    void addAuthorToSecondaryIndexFile(Author &author, map<string, short> &authorsSecondaryIndex)
     {
+        // Convert char array into long long
         long long authorId = LibraryUtilities::convertCharArrToLongLong(author.authorID);
 
+        // Check if the name is unique and is not duplicate
         if (authorsSecondaryIndex.count(author.authorName) == 0)
         {
             cout << "Empty list\n";
+            // Insert into the secondary index with pointer = -1 because it does not point to any primary key
             authorsSecondaryIndex[author.authorName] = addToInvertedList(authorId, -1); // RRN
             cout << "Added value: " << authorsSecondaryIndex[author.authorName] << "\n";
         }
-        else
-        {
+        else // Otherwise, update the value of the RRN of this name to point to the next RRN
             authorsSecondaryIndex[author.authorName] = addToInvertedList(authorId, authorsSecondaryIndex[author.authorName]);
-        }
+
         // Update the status of the file to be NOT up to date, to save it to the disk afterward
         LibraryUtilities::markAuthorsSecondaryIndexFlag('0');
     }
 
-    short addToInvertedList(long long authorId, short nextRecordPointer)
+    // Add the new author id to the inverted list of primary keys & return the RRN of the added record
+    short addToInvertedList(long long &authorId, short nextRecordPointer)
     {
         // Open the file in multiple modes
         fstream invertedList(LibraryUtilities::authorsSecondaryIndexLinkedListFile, ios::in | ios::out | ios::binary);
@@ -123,14 +242,18 @@ public:
         return bestOffset / (sizeof(long long) + sizeof(short)); // RRN
     }
 
+    // Return the byte offset to insert the new record into the inverted list file
     short getBestOffsetInInvertedList()
     {
+        // Open the file in input mode
         fstream invertedList(LibraryUtilities::authorsSecondaryIndexLinkedListFile, ios::in | ios::binary);
 
+        // Get the file size
         invertedList.seekg(0, ios::end);
         int endOffset = invertedList.tellg();
-        invertedList.seekg(0, ios::beg);
 
+        invertedList.seekg(0, ios::beg);
+        // Search to find the offset of the first deleted record to insert into
         while (invertedList)
         {
             long long ISBN;
@@ -149,3 +272,5 @@ public:
         return max(0, endOffset);
     }
 };
+
+#endif // AUTHORS_ADDING_H
