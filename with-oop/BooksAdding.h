d26b72c5c751214870815c1f6b23a826417ecb08
iSeFz <seifyahia@gmail.com>
2023-12-07 21:32:11 +0200

    Added comments & header guards for BooksAdding.h

--- with-oop/BooksAdding.h
+++ with-oop/BooksAdding.h
@@ -1,8 +1,6 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <map>
-#include <cstring>
+#ifndef BOOKS_ADDING_H
+#define BOOKS_ADDING_H
+
 #include "LibraryUtilities.h"
 #include "Book.h"
 
@@ -207,26 +205,30 @@ public:
         LibraryUtilities::markBooksPrimaryIndexFlag('0');
     }
 
+    // Add the new book to the secondary index file
     void addBookToSecondaryIndexFile(Book book, map<long long, short> &booksSecondaryIndex)
     {
+        // Convert char array into long long
         long long authorId = LibraryUtilities::convertCharArrToLongLong(book.authorID);
         long long ISBN = LibraryUtilities::convertCharArrToLongLong(book.ISBN);
 
+        // Check if the author is unique and is not duplicate
         if (booksSecondaryIndex.count(authorId) == 0)
         {
             cout << "Empty list\n";
+            // Insert into the secondary index with pointer = -1 because it does not point to any primary key
             booksSecondaryIndex[authorId] = addToInvertedList(ISBN, -1); // RRN
             cout << "Added value: " << booksSecondaryIndex[authorId] << "\n";
         }
-        else
-        {
+        else  // Otherwise, update the value of the RRN of this name to point to the next RRN
             booksSecondaryIndex[authorId] = addToInvertedList(ISBN, booksSecondaryIndex[authorId]);
-        }
+
         // Update the status of the file to be NOT up to date, to save it to the disk afterward
         LibraryUtilities::markBooksSecondaryIndexFlag('0');
     }
 
-    short addToInvertedList(long long ISBN, short nextRecordPointer)
+    // Add the new book isbn to the inverted list of primary keys & return the RRN of the added record
+    short addToInvertedList(long long &ISBN, short nextRecordPointer)
     {
         // Open the file in multiple modes
         fstream invertedList(LibraryUtilities::booksSecondaryIndexLinkedListFile, ios::in | ios::out | ios::binary);
@@ -257,14 +259,18 @@ public:
         return bestOffset / (sizeof(long long) + sizeof(short)); // RRN
     }
 
+    // Return the byte offset to insert the new record into the inverted list file
     short getBestOffsetInInvertedList()
     {
+        // Open the file in input mode
         fstream invertedList(LibraryUtilities::booksSecondaryIndexLinkedListFile, ios::in | ios::binary);
 
+        // Get the file size
         invertedList.seekg(0, ios::end);
         int endOffset = invertedList.tellg();
+        
         invertedList.seekg(0, ios::beg);
-
+        // Search to find the offset of the first deleted record to insert into
         while (invertedList)
         {
             long long ISBN;
@@ -283,3 +289,5 @@ public:
         return max(0, endOffset);
     }
 };
+
+#endif // BOOKS_ADDING_H
